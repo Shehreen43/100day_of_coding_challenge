465d6a92a578b6b3b9479e91c2a269208d0b7578
Shehreen <160480886+Shehreen43@users.noreply.github.com>
2024-06-06 08:18:36 +0500

    ....

--- Day1-to-10_of100Day_coding/day18/flexible_obj_key.js
+++ Day1-to-10_of100Day_coding/day18/flexible_obj_key.js
@@ -3,16 +3,13 @@
 Question 54: Making Flexible Object Keys: Learn how to set up a list where you can change the name of each section based on
  what you need at that moment, like adjusting labels based on user choices.
 */
-
 // A way to make a flexible list
-function createObjectWithDynamicKey(key: string, value: string) {
-    let dynamicObject = {};
+function createObjectWithDynamicKey(key, value) {
+    var dynamicObject = {};
     // Setting up a section in the list with a changeable name
     return dynamicObject;
 }
-
 // Using the flexible list setup for a user's preference
-let userPreference = createObjectWithDynamicKey("theme", "dark");
-
+var userPreference = createObjectWithDynamicKey("theme", "dark");
 // Showing the user's choice
 console.log(userPreference);
