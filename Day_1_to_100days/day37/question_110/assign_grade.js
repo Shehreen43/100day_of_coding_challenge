0b854d62163a1b69598d5d8b6a2f5edbb4974fb6
Shehreen <160480886+Shehreen43@users.noreply.github.com>
2024-07-06 11:48:57 +0500

    ....

--- Day_1_to_100days/day37/question_110/assign_grade.js
+++ Day_1_to_100days/day37/question_110/assign_grade.js
@@ -2,20 +2,21 @@
 /*
 Create a function that assigns a grade (A, B, C, D, F) based on a students score.
 */
-function assignsGrade(score: number) {
-    if (score >= 80 && score <100) {
-
+function assignsGrade(score) {
+    if (score >= 80 && score < 100) {
         return 'Your Grade is A';
-    } else if (score >= 70 && score <80) {
+    }
+    else if (score >= 70 && score < 80) {
         return 'Your Grade is B';
-    } else if (score >= 60 && score <70) {
+    }
+    else if (score >= 60 && score < 70) {
         return 'Your Grade is C';
-    } else if (score >= 50 && score <60) {
+    }
+    else if (score >= 50 && score < 60) {
         return 'Your Grade is D';
-    } else {
+    }
+    else {
         return 'Your Grade is F';
     }
 }
-
 console.log(assignsGrade(95));
-
---
+++ Day_1_to_100days/day37/question_111/age_group.js
@@ -0,0 +1,18 @@
+//                    QUESTION: 111
+/*
+Use an if-else-if chain to categorize a persons age group (child, teenager, adult).
+*/
+function categorizeAge(personsage) {
+    if (personsage < 13) {
+        return "The person is a child.";
+    }
+    else if (personsage < 20) {
+        return "The person is a teenager.";
+    }
+    else {
+        return "The person is an adult.";
+    }
+}
+console.log(categorizeAge(7));
+console.log(categorizeAge(18));
+console.log(categorizeAge(24));
